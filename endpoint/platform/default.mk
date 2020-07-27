4b337c8e84a72fea2619d38597dd4474f1277407
HYChang <HYChang@biilabs.io>
2020-07-27 12:33:36 +0800

    Can build with mkexe

--- endpoint/platform/default.mk
+++ endpoint/platform/default.mk
@@ -4,26 +4,33 @@
 # terms of the MIT license. A copy of the license can be found in the file
 # "LICENSE" at the root of this distribution.
 
-export PA_DIR=${LEGATO_ROOT}/platform
+# need to configelgato first
+export PA_DIR=${LEGATO_ROOT}/platformAdaptor
+export LEGATO_MODEM_SERVICES=${LEGATO_ROOT}/components/modemServices
+export LEGATO_SECSTORE=${LEGATO_ROOT}/components/secStore
+export ENDPOINT_DIR=$(PWD)/endpoint
 
 platform-build-command = \
-	source ./legato/bin/configlegatoenv && \	
 	cd endpoint && \
-	mkexe \ 
-	-o endpoint_test \
-	-w endpoint_build \
-	-i endpoint/endpointComp \
+	mkexe -o endpoint_test \
+	-w endpoint_test_build \
 	-t $(EP_TARGET) \
-    -i ${LEGATO_ROOT}/framework/liblegato/ \
-    -i ${LEGATO_ROOT}/interfaces/modemServices/ \
-    -i ${LEGATO_ROOT}/interfaces/ \
-	-l /home/splasky/Desktop/Biilabs/workspace/tangle-accelerator/endpoint/ \
-	-C -static \
-    ${CFLAGS} \
-    ${LFLAGS} \
+	-i ${LEGATO_ROOT}/interfaces \
+	-i ${LEGATO_ROOT}/interfaces/secureStorage \
+	-i ${LEGATO_ROOT}/interfaces/modemServices \
+	-i ${LEGATO_SECSTORE}/platformAdaptor/inc \
+	-i ${LEGATO_MODEM_SERVICES}/modemDaemon \
+	-i ${LEGATO_ROOT}/framework/liblegato \
+	-i ${LEGATO_ROOT}/framework/liblegato/linux \
+	-i ${LEGATO_ROOT}/components/appCfg \
+    	-i ${LEGATO_ROOT}/components/cfgEntries \
+	-i ${PA_DIR}/simu/components/le_pa_secStore \
+    	-i ${PA_DIR}/simu/components/le_pa \
+	-i ${PA_DIR}/simu/components/simuConfig \
 	$(LEGATO_FLAGS) \
-    -L "-ljansson" \
-	endpointComp
+	endpointComp \
+	${PA_DIR}/simu/components/le_pa_secStore \
+	${PA_DIR}/simu/components/simuConfig
 
 	# mkapp -t $(EP_TARGET) $(LEGATO_FLAGS) endpoint.adef
 	#leaf shell -c "mkapp -t $(EP_TARGET) $(LEGATO_FLAGS) endpoint.adef"
